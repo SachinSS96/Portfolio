f029123ec7d9636cf1801cbbb68716d9b2c15464
Sachin Sharma <130166959+SachinSS96@users.noreply.github.com>
2023-07-11 19:58:29 +0530

    Update Apple_Store.sql

--- Apple Store Dataset and SQL code/Apple_Store.sql
+++ Apple Store Dataset and SQL code/Apple_Store.sql
@@ -86,9 +86,9 @@ from(
 where a.rank = 1
 
 
-	----1. paid apps  have better ratings 
-	----2. apps supporting between 10 and 30  languages  have better  ratings 
-	----3. finance and books  apps have  low  ratings 
-	----4. apps with a longer description  have better ratings
-	----5. a new app should aim for  an average  rating above 3.5
-	----6. games and entertainment have higher competition 
+	----1. Paid apps have better ratings 
+	----2. Apps supporting between 10 and 30 languages have better ratings 
+	----3. Finance and books apps have low ratings 
+	----4. Apps with a longer description have better ratings
+	----5. A new app should aim for an average rating above 3.5
+	----6. Games and entertainment have higher competition 
